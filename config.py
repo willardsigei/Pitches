7776e34ecdde91eddd834a8490af75ff36519aa6
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-06 14:01:17 +0300

    base.html

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/login'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitches'
     SECRET_KEY =os.environ.get('SECRET_KEY')
-   
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
    
     #  email configurations
   
@@ -19,7 +19,7 @@ class TestConfig(Config):
     DEBUG = True
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/login'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitches'
     DEBUG = True
 
 
