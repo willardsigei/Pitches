6541308b63a26b5cfb36c21b32b14f0e8a77401f
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-06 12:01:12 +0300

    mail validations

--- config.py
+++ config.py
@@ -5,6 +5,22 @@ class Config:
     SECRET_KEY =os.environ.get('SECRET_KEY')
    
    
+    #  email configurations
+  
+    
+
+
+class ProdConfig(Config):
+   pass
+
+
+class TestConfig(Config):
+    
+    DEBUG = True
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/login'
+    DEBUG = True
 
 
 
