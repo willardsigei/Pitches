7245cd5c483fd15456e19e56933f7c8bdf041839
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-06 12:20:26 +0300

    password verificaion

--- app/models.py
+++ app/models.py
@@ -20,3 +20,17 @@ class User(UserMixin,db.Model):
     @password.setter
     def password(self, password):
         self.password_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
+
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+        
