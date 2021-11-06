3ee75c42ddf4cd1840d0437acc74cf6e8cedf4b7
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-06 12:24:16 +0300

    initialised flask extentions and reg blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -30,3 +30,25 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+
+        # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+ 
+#Registering the blueprint
+
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+
+    return app
