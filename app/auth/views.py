5867326cd9309dcaecd526eaedebd0bd428dc2db
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-06 09:44:30 +0300

    configured database

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,3 +14,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/login'
+    SECRET_KEY =os.environ.get('SECRET_KEY')
+   
+   
