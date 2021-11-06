6ba092d1dea446f56867db2ddc0c8fbc53d478fc
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-06 12:03:18 +0300

    loginform

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -21,3 +21,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken') 
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
